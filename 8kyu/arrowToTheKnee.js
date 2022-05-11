2cf0f884de8b6eba857fac3db012443e05698848
Merrick Cho <merrick.cho@gmail.com>
2022-05-11 01:02:36 -0700

    Daily code challenge for 2022-05-11

---
+++ 8kyu/arrowToTheKnee.js
@@ -0,0 +1,7 @@
+// Take an Arrow to the knee, Functionally
+// https://www.codewars.com/kata/559f3123e66a7204f000009f
+//
+
+var ArrowFunc = function(arr) {
+    return arr.map( e => String.fromCharCode(e)).join(''); //Complete this function
+  }
