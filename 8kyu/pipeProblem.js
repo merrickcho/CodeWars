45da6f3f1b6e3faeff5fa5e3c5f511fdf2a785bc
Merrick Cho <merrick.cho@gmail.com>
2022-05-13 01:04:16 -0700

    Daily code challenge for 2022-05-13

---
+++ 8kyu/pipeProblem.js
@@ -0,0 +1,12 @@
+// Lario and Muigi Pipe Problem
+// https://www.codewars.com/kata/56b29582461215098d00000f
+//
+
+function pipeFix(n){
+
+    let res = []
+    for(i=n[0];i<=n[n.length-1];i++){
+      res.push(i)
+    }
+      return res
+    }
