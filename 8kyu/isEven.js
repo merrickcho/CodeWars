18cd558f93a092b1949ea8834056c5da8796abf8
Merrick Cho <merrick.cho@gmail.com>
2022-10-15 01:24:06 -0700

    Daily code challenge for 2022-10-15

---
+++ 8kyu/isEven.js
@@ -0,0 +1,14 @@
+// Is it even?
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+//
+
+function testEven(n) {
+    //p
+    //We have variable of n
+    //r
+    //return a boolean of true or false
+    //e
+    //we will use the modular function to determine if n is even by check if the modular is 0
+    //p
+    return n%2==0? true: false
+  }
