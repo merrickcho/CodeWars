73d074c8307a4452ca51faebc85268fefd52e2a4
Merrick Cho <merrick.cho@gmail.com>
2022-10-30 01:37:48 -0700

    Daily code challenge for 2022-10-30

---
+++ 8kyu/wideMouthedFrog.js
@@ -0,0 +1,7 @@
+// The Wide-Mouthed frog!
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+//
+
+function mouthSize(animal) {
+    return animal.toLowerCase().includes('alligator')?'small':'wide'
+  }
